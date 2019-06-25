1ff5d2bda69634aeccce43efa339db1a96b512ba
Rafael Montenegro <rafaelgsmontenegro@gmail.com>
2019-06-24 19:48:11 -0700

    Removed hamburger menu

--- www/js/maps.js
+++ www/js/maps.js
@@ -17,6 +17,7 @@ var markers = [];
 var pos;
 
 function initMap() {
+
     map = new google.maps.Map(document.getElementById('map'), {
         // center: {lat: -34.397, lng: 150.644},
         zoom: 12,
